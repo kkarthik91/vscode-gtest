7b423e0cac4a1f9f7f2187e4659bfc7de06f6cba
kkarthik91 <144001309+kkarthik91@users.noreply.github.com>
2023-09-05 12:29:59 +0530

    Update CarVehicleSystem.cpp

--- CarVehicleSystem.cpp
+++ CarVehicleSystem.cpp
@@ -8,10 +8,24 @@ public:
   string regno;
 };
 
-class Car {
+class StartupMotor{};
+class FluePump {};
+class Engine{
 private:
+    FuelPump fuelPump;
+    StartupMotor motorObj;
 public:
-  
+    void crankEngine(){}
+    
 };
 
-class Engine{};
+class Car : public Vehicle {
+    Engine engObj;
+public:
+    void start(){
+      engObj.crankEngine();
+    }
+};
+
+
+
